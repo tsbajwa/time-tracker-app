dce5b512d535a09c0b9de7bea04999ab9dcaf2c5
tsbajwa <teranjit@gmail.com>
2017-08-14 10:24:55 +1000

    Added basuc server port listening

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set('port', (process.env.API_PORT || 3001));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
